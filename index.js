d0dc90426531555565d04239df9ef1b44e2fe7d7
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-05-18 13:05:29 +0300

    payload explained

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer(function(req,res){
@@ -25,12 +26,26 @@ const server = http.createServer(function(req,res){
   // Get the HTTP method
   const method = req.method.toLowerCase();
 
-  // A response
-  res.end("Hello Node.js\n");
+  // Headers
+  const headers = req.headers;
+
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+
+  req.on('data', function (data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end',function () {
+    buffer += decoder.end();
+
+    // A response
+    res.end("Hello Node.js\n");
 
 
-  // Log the request path
-  console.log('Request path : '+trimmedPath+' Request method: '+method+' Query parametres: ', queryStringObject);
+    // Log the request path
+    console.log('This is the payload: ', buffer);
+  });
 });
 
 server.listen(3000, function(){
