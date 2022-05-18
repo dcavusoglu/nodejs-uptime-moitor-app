acfba2cbba3191b38dccfa2d0eb29c218f35d529
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-05-18 12:38:15 +0300

    node v8

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 /*
 * Primary file for the API
 *
+*
 */
 
 // Dependencies
-
 const http = require('http');
 const url = require('url');
 
-
 // The server should respond to all requests with a string
+const server = http.createServer(function(req,res){
 
-const server = http.createServer(function(req,res) {
+  // Get the url
+  const parsedUrl = url.parse(req.url,true);
 
-  // Get the URL and parse it
-  const parsedURL = url.parse(req.url,true);
-
-  // Get the path
-  const path = parsedURL.pathname;
+  // get the path
+  const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
-  // Send the response
-  res.end('Hello Node\n');
+  // Get query string as an object
+  const queryStringObject = parsedUrl.query;
+
+
+  // Get the HTTP method
+  const method = req.method.toLowerCase();
+
+  // A response
+  res.end("Hello Node.js\n");
+
 
   // Log the request path
-  console.log('Request received on this path: '+trimmedPath);
+  console.log('Request path : '+trimmedPath+' Request method: '+method+' Query parametres: ', queryStringObject);
 });
-
-// Start the server, and have it listen on port 3000
 
 server.listen(3000, function(){
-  console.log("The server is listening on port 3000 now");
-});
+  console.log('The server is listening on post 3000 now');
+})
+
+console.log('Hello Duygu!');
