0cb0a20683b015d83f544162efbf2ad203e85683
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-05-19 13:49:04 +0300

    post and get requests completed

--- index.js
+++ index.js
@@ -9,16 +9,11 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
+const config = require('./lib/config');
 const fs = require('fs');
-const _data = require('./lib/data');
 const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
 
-
-//TESTING
-_data.delete('test', 'newFile', function(err){
-  console.log('Error: ', err);
-});
 
 
 // Instantiating the http server
@@ -90,7 +85,7 @@ const unifiedServer = function(req, res) {
       'queryStringObject' : queryStringObject,
       'method' : method,
       'headers' : headers,
-      'payload' : buffer
+      'payload' : helpers.parseJsonToObject(buffer)
     };
 
     // Route the request to the handler specified in the router
