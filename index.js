7945279d2d15b874bce2d2aac96b5fc5d488bd8d
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-06-01 10:22:20 +0300

    check services post

--- index.js
+++ index.js
@@ -114,7 +114,8 @@ const unifiedServer = function(req, res) {
 const router = {
   'ping' : handlers.ping,
   'users' : handlers.users,
-  'tokens' : handlers.tokens
+  'tokens' : handlers.tokens,
+  'checks' : handlers.checks
 };
 
 console.log('Hello Duygu!');
